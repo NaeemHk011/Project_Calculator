3e8ead8d5ad20ce9eeadb6c5a60bff16529e98fd
NaeemHk011 <itxkhaskheli@gmail.com>
2024-02-21 14:25:44 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,45 @@
+// interface stud {
+//     name: string,
+//     age: number,
+//     RollNo: number
+// }
+
+// interface stud {
+//     name: string,
+
+// }
+
+// const student: stud = {
+//     name: "Syed Kamal",
+//     age: 30,
+//     RollNo: 94393
+// }
+
+// const student2: stud = {
+//     name: "ahmed",
+//     age: 20,
+//     RollNo: 944393
+// }
+
+// const student3: stud = {
+//     name: "ahmed",
+//     age: 20,
+//     RollNo: 944393
+// }
+
+// Union types 
+
+// let RollNo: number | string = 8473874;
+// RollNo = "piaic87474"
+
+
+// let func: boolean | number | string = true;
+// func = 10
+// func = "Naeem"
+
+// // literal type 
+
+// let Academy:"CCAP" = "CCAP"
+
+// Academy = "Piaic"
+
